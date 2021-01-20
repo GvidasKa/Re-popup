3c9cfcfae52eb8e03154a93e80561041207e154c
Gvidas Kairys <gvidas@scoding.com>
2021-01-20 20:11:41 +0200

    Setting global plugin settings in re-popup.php, so wordpress would recognize it. Creating index.php to prevent outsiders from accessing plugin files.

---
+++ wp-content/plugins/re-popup/re-popup.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package RepopupPlugin
+ */
+/*
+ * Plugin Name: Re Popup
+ * Plugin URI: https://github.com/GvidasKa/Re-popup
+ * Description: This is a popup plugin, which allows admin to create, read, update and delete pop-ups. These pop-ups appear when a visitor tries to leave the page I.e. user moves the mouse outside the window.
+ * Version: 1.0.0
+ * Author: Gvidas Kairys
+ * Author URI: https://github.com/GvidasKa/
+ * License: GPLv2 or later
+ * Text Domain: re-popup
+ */
